4a33425782002c918fd13afdd6770edda7d2da2f
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-08-17 13:01:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const codeInputs = document.querySelectorAll('.code');
+
+codeInputs.forEach((input, index) => {
+  input.addEventListener('keyup', (event) => {
+    const key = event.key;
+    const nextInput = codeInputs[index + 1];
+    const prevInput = codeInputs[index - 1];
+
+    // Allow only numbers
+    if (key !== 'Backspace' && key !== 'Delete' && (!/^\d$/.test(key))) {
+      event.preventDefault();
+      return;
+    }
+
+    if (key === 'Backspace') {
+      if (prevInput) {
+        prevInput.focus();
+        prevInput.value = '';
+      }
+    } else if (nextInput) {
+      nextInput.focus();
+    }
+  });
+});
